ea17d975b1a2f9aa328212453dc407b19af749b5
Spencer <spencerb242@proton.me>
2023-01-05 18:52:59 -0700

    added logic to determine when cards are matched and to keep them flipped

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import "./App.css";
 import SingleCard from "./components/SingleCard";
 
 const cardImages = [
-  { src: "/img/helmet-1.png" },
-  { src: "/img/potion-1.png" },
-  { src: "/img/ring-1.png" },
-  { src: "/img/scroll-1.png" },
-  { src: "/img/shield-1.png" },
-  { src: "/img/sword-1.png" },
+  { src: "/img/helmet-1.png", matched: false },
+  { src: "/img/potion-1.png", matched: false },
+  { src: "/img/ring-1.png", matched: false },
+  { src: "/img/scroll-1.png", matched: false },
+  { src: "/img/shield-1.png", matched: false },
+  { src: "/img/sword-1.png", matched: false },
 ];
 
 function App() {
@@ -35,12 +35,26 @@ function App() {
 
   useEffect(() => {
     if (choiceTwo) {
-      choiceOne.src === choiceTwo.src
-        ? console.log("Match")
-        : console.log("No Match");
-      resetTurn();
+      if (choiceOne.src === choiceTwo.src) {
+        console.log("Match");
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        resetTurn();
+      } else {
+        console.log("No Match");
+        resetTurn();
+      }
     }
   }, [choiceTwo]);
+
+  console.log(cards);
 
   const resetTurn = () => {
     setChoiceOne(null);
@@ -55,7 +69,12 @@ function App() {
 
       <div className="card-grid">
         {cards.map((card) => (
-          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+          />
         ))}
       </div>
     </div>
